df48ee96b52b8f986c1ec72e538f148f4765ed45
Jared Mallas <jaredmallas@northwesternmutual.com>
2018-11-08 16:24:58 -0600

    Initial commit

---
+++ src/com/company/Student.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Student extends Address {
+    private String name;
+    private int age;
+    private Address address;
+
+    public Student(String streetAddress, int zipCode, String state, String phone, String name, int age) {
+        super(streetAddress, zipCode, state, phone);
+        this.name = name;
+        this.age = age;
+        this.address = new Address(streetAddress, zipCode, state, phone);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Address getAddress()
+    {
+        return address;
+    }
+}
